5405a10f8f51c26fd4b48f2d5be42310439715f1
青衫 <uouoio@qq.com>
2020-06-20 15:37:46 +0800

    Update 2020-06-20T15:37:46.846478

--- count.py
+++ count.py
@@ -1,23 +1,24 @@
 import datetime
+import random
 import os
 
-def change_day(): 
-  return datetime.timedelta(days=1) 
 
-def change_time(): 
-  return datetime.timedelta(minutes=1) 
+def change_day():
+    return datetime.timedelta(days=1)
 
-now = datetime.datetime.now() 
-commit_date = (now.replace(year=now.year-1) + change_day()) 
 
-while commit_date < now: 
-  commit_date = commit_date + change_day() 
-  for i in range(5): 
-    f = open('data.txt', 'a+') 
-    commit_date = commit_date + change_time() 
-    f.writelines(commit_date.isoformat() + '\n') 
+def change_time():
+    return datetime.timedelta(minutes=1)
+
+
+# 循环的时间
+commit_date = datetime.datetime.now()
+
+for i in range(random.randint(1, 18)):
+    f = open('data.txt', 'a+')
+    commit_date = commit_date + change_time()
+    f.writelines(commit_date.isoformat() + '\n')
     f.close()
-    os.system('git add .') 
+    os.system('git add .')
     os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
-
 os.system('git push')
